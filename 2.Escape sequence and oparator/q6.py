11e1292a28c0b474c6d11121064d1f25bf2b2f70
Puja kumari <pujakumari11832@gmail.com>
2026-08-12 12:59:59 +0530

    q6 solve

--- 2.Escape sequence and oparator/q6.py
+++ 2.Escape sequence and oparator/q6.py
@@ -2,3 +2,10 @@
 , calculate and print their product
 using += in a way that adds the first number to itself the
 second number of times. (Think carefully.)"""
+
+num1=int(input("Enter the first number= "))
+num2=int(input("Enter the second number= "))
+product=0
+for i in range(1,num2+1):
+    product=product+num1
+print(f"product of {num1} of {num2} without using * = {product}")
