a456460c400a4554cda89dafd7168e167d387a5a
Puja kumari <pujakumari11832@gmail.com>
2026-08-16 04:23:32 +0530

    function called square

---
+++ 8.function/q41_return_statement.py
@@ -0,0 +1,14 @@
+"""
+Write a function called square that takes a number and returns its square.
+Store the result and print it.
+"""
+#1st methhod
+def square(num):
+    return num*num
+result=square(10)
+print(result)
+
+#2nd method 
+def square(num):
+    return num**2
+print(square(5))
