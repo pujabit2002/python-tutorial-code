b97a69597cca91f6c0c446347af2c73aef60c54c
Puja kumari <pujakumari11832@gmail.com>
2026-08-16 05:11:56 +0530

    Default argument done

---
+++ 8.function/6.default_arguments.py
@@ -0,0 +1,9 @@
+def calculate_marks(maths, eng, hindi,comp=0,history=0 ):
+    print(f"maths = {maths}")
+    print(f"eng = {eng}")
+    print(f"hindi = {hindi}")
+    print(f"comp = {comp}")
+    print(f"history = {history}")
+    total_marks=maths, eng, hindi, comp, history
+    print(f"Total marks scored = {total_marks}")
+calculate_marks(12,32,11,19)
