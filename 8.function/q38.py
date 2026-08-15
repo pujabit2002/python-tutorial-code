54561955c9f9c03de56c9bfe973f1a6f1c7266db
Puja kumari <pujakumari11832@gmail.com>
2026-08-15 06:15:31 +0530

    q38 done

---
+++ 8.function/q38.py
@@ -0,0 +1,9 @@
+"""
+Write a function called rectangle_area that takes length and breadth
+as parameters and prints the area.
+"""
+def reactangle_area(length,breadth):
+    area=length*breadth
+    print(f"area = {area}")
+reactangle_area(10,20)
+reactangle_area(20,30)
