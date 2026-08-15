ef123881b8b2ce0f3e1216feba9dba751b2cb165
Puja kumari <pujakumari11832@gmail.com>
2026-08-15 15:04:04 +0530

    return flow done

---
+++ 8.function/4.return_flow.py
@@ -0,0 +1,11 @@
+def greet(name, age):
+    return f"your name is {name} and age is {age} years"
+print(greet("Anirudh",20))
+
+#2nd method
+def greet(name,age):
+    return f"your name is {name} and age is {age} years"
+    print("Good")
+    print("Bye")
+    print("Done")
+print(greet("Anirudh",20))
