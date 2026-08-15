611a2cd13e77d8274689abf16348723800961000
Puja kumari <pujakumari11832@gmail.com>
2026-08-15 06:03:02 +0530

    Ask a name, age,gender,print done

--- 8.function/2.parameters_and _arguments.py
+++ 8.function/2.parameters_and _arguments.py
@@ -4,3 +4,18 @@ def add (a,b,c):
     print(f"total = {ans}")
 add(10,12,45)
 add(17,28,49)
+
+
+# Ask a name, age,gender,print
+#1st method
+def greet(name, age, gender):
+    print(f"Hey {name}! your age is {age} and gender is {gender}")
+greet("puja", 18,"female")
+
+#2nd method
+def greet(name, age, gender):
+    print(f"Hey {name}! your age is {age} and gender is {gender}")
+n=input("Enter name= ")
+a=int(input("Enter age= "))
+g=input("Enter gender= ")
+greet(n,a,g)
