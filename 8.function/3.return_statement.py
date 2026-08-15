2402fb0bebc705f798b41ff8ece201018aaa3622
Puja kumari <pujakumari11832@gmail.com>
2026-08-15 14:56:54 +0530

    retturn statement basic question done

---
+++ 8.function/3.return_statement.py
@@ -0,0 +1,23 @@
+"""
+def addition (n1,n2,n3):
+    return (n1+n2+n3)
+ans=addition(20,30,40)
+print(ans)
+"""
+
+#2nd method
+def addition (n1,n2,n3):
+    print(n1+n2+n3)
+ans=addition(10,20,30)
+print(ans)
+
+
+
+#True or False return, if user can vote or not
+"""def can_vote(age):
+    if age>=18:
+        return True
+    else:
+        return False
+ans =can_vote(80)
+print(ans) """    
