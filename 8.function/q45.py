b91d260eb0bc21716798c45999061fbaa95b8304
Puja kumari <pujakumari11832@gmail.com>
2026-08-16 06:33:41 +0530

    q45 done

---
+++ 8.function/q45.py
@@ -0,0 +1,6 @@
+"""
+Write a lambda function that takes a number and returns
+"Positive", or "Negative"
+"""
+positive_negative=lambda num : "Positive" if num>0 else "negative"
+print(positive_negative(56)) 
