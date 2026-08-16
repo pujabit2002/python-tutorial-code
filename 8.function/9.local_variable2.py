304afa8f395ae55c4ee1157a50b7992dbe9511ce
Puja kumari <pujakumari11832@gmail.com>
2026-08-16 05:36:07 +0530

    local variable

---
+++ 8.function/9.local_variable2.py
@@ -0,0 +1,29 @@
+#1st
+def xyz(n1,n2):
+    n1=100
+    n2=200
+    print(f"Inside function n1={n1} and n2={n2}")
+xyz(10,20)
+
+#2nd
+def xyz (n1,n2):
+    n1=100
+    n2=200
+    print(f"Inside function n1={n1} and n2={n2}")
+a=10
+b=20
+xyz(a,b)
+print(a)
+print(b)
+
+#3rd
+def xyz(n1,n2):
+    n1=100
+    n2=200
+    print("Inside function n1={n1} and n2={n2}")
+n1=10
+n2=20
+xyz(n1,n2)
+print(n1)
+print(n2)
+
