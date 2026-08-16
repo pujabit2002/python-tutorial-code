e859d5663838a618da8f8009fa6085d83eb4456c
Puja kumari <pujakumari11832@gmail.com>
2026-08-16 05:46:13 +0530

    Global

---
+++ 8.function/10.global.py
@@ -0,0 +1,13 @@
+#1st
+name="Anirudh"
+def greet():
+    print(f"Hey {name}! Good morning")
+greet()
+
+#2nd 
+name="Anirudh"
+def greet():
+    name="Muskan"
+    print(f"Hey {name}! Good morning")
+greet()
+print(name)
