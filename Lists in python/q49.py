f771daf456a3854151dee243e0819a11e0a985f5
Puja kumari <pujakumari11832@gmail.com>
2026-08-16 14:00:05 +0530

    q49 done

---
+++ Lists in python/q49.py
@@ -0,0 +1,11 @@
+"""
+Create a list of 5 of your favourite movies. Print the first, last, and middle movie from your list using both positive and negative indexing where appropriate.
+"""
+movies=["Housefull", "Dhurandhar", "Border", "Dhamal", "kakuda"]
+print(movies[0])
+print(movies[2])
+print(movies[4])
+
+print(movies[-1])
+print(movies[-3])
+print(movies[-5])
