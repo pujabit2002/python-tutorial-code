6dbc09e0932a9867fb8825caa3354961a695b4e8
Puja kumari <pujakumari11832@gmail.com>
2026-08-16 14:04:18 +0530

    q50 done

---
+++ Lists in python/q50.py
@@ -0,0 +1,7 @@
+"""
+Create a list of 5 numbers (e.g., [10, 20, 30, 40, 50]). Replace the second and fourth elements of this list with the number 0 using indexing. Print the updated list.
+"""
+lst=[10,20,30,40,50]
+lst[2]=0
+lst[4]=0
+print(lst)
