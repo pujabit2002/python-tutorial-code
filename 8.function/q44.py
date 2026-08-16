a6eb67418b831f99d9283eacefade663324d8ced
Puja kumari <pujakumari11832@gmail.com>
2026-08-16 06:32:44 +0530

    q44 done

--- 8.function/q44.py
+++ 8.function/q44.py
@@ -2,5 +2,5 @@
 Write a lambda function that takes a number and returns its cube. Store
 it in a variable and call it.
 """
-cube=lambda num : "Positive" if num>0 else "negative"
-print(cube(56))
+cube= lambda num: num*num*num
+print(cube(3))
