8efc69db473cb37727eb911eff6c9e86437f0e76
Puja kumari <pujakumari11832@gmail.com>
2026-08-16 13:09:11 +0530

    creating list

---
+++ Lists in python/1.creating_list.py
@@ -0,0 +1,16 @@
+#creating a list
+names=["Anirudh", "Rahul","Nihar", 55, 48.112, True, False]
+print(names)
+print(type(names))
+
+#List operation-> -,+,*,/
+marks=[3,7,88,67,50]
+print(marks*4)
+# + operation kaam nhi karta hai list mai->   print(marks +4)
+# - opartion kaam nhi karta hai list mai->.    print(marks-4)
+# / operation kaam nhi karta hai list mai->.  print(marks/4)
+
+
+mark=[1,2,3,4]
+mark2=[4,2,6]
+print(mark+mark2)
