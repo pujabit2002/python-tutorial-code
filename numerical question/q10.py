7d57155c0d9a0027f7014473f8e0da8299564ef0
Puja kumari <pujakumari11832@gmail.com>
2026-08-18 22:23:48 +0530

    q10 done

---
+++ numerical question/q10.py
@@ -0,0 +1,12 @@
+"""Take a year as input. Check if it is a leap year. A year is a leap
+year if it is divisible by 4, but not by 100, unless it is also
+divisible by 400.
+"""
+
+def check_leap_year():
+    year=int(input("Enter the year"))
+    if (year%4==0 and year%100!=0) or (year%400==0):
+        print("year is leap year")
+    else:
+        print("not leap year")
+check_leap_year()
