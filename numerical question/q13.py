1910e0c685479101a53ab970242cb5923690c6a6
Puja kumari <pujakumari11832@gmail.com>
2026-08-18 22:32:55 +0530

    q13 done

---
+++ numerical question/q13.py
@@ -0,0 +1,9 @@
+# Print all the numbers which are divisible by 3 and 5, from 1 to 100.
+def print_all_the_numbers():
+    num=int(input("enter the number"))
+    i=1
+    while(i<=100):
+        if (i%3==0 and i%5==0):
+            print(i)
+        i=i+1
+print_all_the_numbers()
