a5f1fb6f831055aec0f16bb3e897be153535b19b
Puja kumari <pujakumari11832@gmail.com>
2026-08-19 15:18:42 +0530

    iteration using for loop

---
+++ Lists in python/7.iteration_using_for_loop.py
@@ -0,0 +1,37 @@
+"""
+nums=[2, 4, 23, 26, 49, 59, 20, 46, 59, 89]
+n=len(nums)
+for i in range(0,n):
+    print(nums[i], end=" ")
+"""
+
+
+
+"""
+nums=[2, 4, 23, 26, 49, 59, 20, 46, 59, 89]
+n=len(nums)
+total=0
+for i in range(0,n):
+    total=total+nums[i]
+print(total)
+"""
+
+"""
+nums=[2, 4, 23, 26, 49, 59, 20, 46, 59, 89]
+for i in nums:
+    print(i,end=" ")
+    """
+
+
+"""
+nums=[2, 4, 23, 26, 49, 59, 20, 46, 59, 89]
+total=0
+for num in nums:
+    total=total+num
+print(total)
+"""
+
+
+nums=[2, 4, 23, 26, 49, 59, 20, 46, 59, 89]
+for num in nums[::-1]:
+    print(num,end=" ")
