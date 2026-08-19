c6b3368daa0518b1f88d5b896c94a87db52ee582
Puja kumari <pujakumari11832@gmail.com>
2026-08-19 15:06:53 +0530

    iteration using while loop

---
+++ Lists in python/6.iteration.using.while.loop.py
@@ -0,0 +1,30 @@
+"""
+nums=[2, 4, 23, 26, 49, 59, 20, 46, 59, 89]
+n=len(nums)
+i=0
+while i<n-1:
+    print(nums[i], end= " ")
+    i=i+1
+"""
+
+
+"""
+nums=[2, 4, 23, 26, 49, 59, 20, 46, 59, 89]
+n=len(nums)
+i=0
+count=0
+while i<=n-1:
+    if nums[i]%2==0:
+        count+=1
+    i+=1
+print(count)
+"""
+
+
+
+nums=[2, 4, 23, 26, 49, 59, 20, 46, 59, 89]
+n=len(nums)
+i=n-1
+while i>=0:
+    print(nums[i], end= " ")
+    i=i-1
