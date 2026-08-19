5055dc1ce2f301ae2e527511ab4c296f14af2085
Puja kumari <pujakumari11832@gmail.com>
2026-08-19 13:10:53 +0530

    add negative slicing

--- Lists in python/5.slicing.py
+++ Lists in python/5.slicing.py
@@ -1,17 +1,35 @@
-lst=[34,2,5,49,60,20,39,10,29,39,40,73,59]
-lst1=lst[1:14]
+#    -13-12 -11 -10-9--8 -7 -6 5- -4 -3 -2 -1
+#     0  1 2 3   4. 5 6.  7 8. 9. 10 11 12
+#lst=[34,2,5,49,60,20,39,10,29,39,40,73,59]
+#lst1=lst[1:14]
+#print(lst1)
+#lst1=lst[5:10]
+#print(lst1)
+#lst1=lst[3:11]
+#print(lst1)
+#lst1=lst[1:88]
+#print(lst1)
+#lst1=lst[5:6]
+#print(lst1)
+#lst1=lst[5:5]
+#print(lst1)
+#lst1=lst[4:]
+#print(lst1)
+#lst1=lst[:3]
+#print(lst1)
+#lst1=lst[0:9:2]
+#print(lst1)
+#lst1=lst[0::2]
+#print(lst1)
+
+#.  -10 -9  -8. -7. -6.  -5. -4. -3. -2. -1
+#.   0.  1.  2.  3.  4.   5.  6.  7.  8.  9
+lst=[56, 78, 89, 23, 49, 69, 38, 45, 28, 29]
+lst1=lst[9:3:-1]
 print(lst1)
-lst1=lst[5:10]
+lst1=lst[5:1:-1]
 print(lst1)
-lst1=lst[3:11]
+lst1=lst[::-1]
 print(lst1)
-lst1=lst[1:88]
-print(lst1)
-lst1=lst[5:6]
-print(lst1)
-lst1=lst[5:5]
-print(lst1)
-lst1=lst[4:]
-print(lst1)
-lst1=lst[:3]
+lst1=lst[::-2]
 print(lst1)
