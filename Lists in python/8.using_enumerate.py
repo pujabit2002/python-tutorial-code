3928349e730937824116be50c5a37f744b5fb735
Puja kumari <pujakumari11832@gmail.com>
2026-08-19 15:22:58 +0530

    using enumerate

---
+++ Lists in python/8.using_enumerate.py
@@ -0,0 +1,9 @@
+nums=[2, 4, 23, 26, 49, 59, 20, 46, 59, 89]
+for index,value in enumerate(nums):
+    print(f"Index = {index} and value = {value}")
+
+
+nums=[2, 4, 23, 26, 49, 59, 20, 46, 59, 89]
+for index, value in enumerate(nums):
+    if value%2==0:
+        print(index)
