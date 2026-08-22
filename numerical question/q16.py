2211e731c7cfd6334060682690984d9683ea35e9
Puja kumari <pujakumari11832@gmail.com>
2026-08-22 21:11:32 +0530

    q16 done

---
+++ numerical question/q16.py
@@ -0,0 +1,13 @@
+"""
+Take a number as input. Print whether it is positive, negative, or zero.
+
+"""
+def positive_negative_zero():
+    number=int(input("Enter any nnumber"))
+    if (number>0):
+        print("Positive")
+    elif (number<0):
+        print("negative")
+    else:
+        print("Zero number0")
+positive_negative_zero()
