b05d9207b29e4fc2fd80de8570f95248a3fa16c7
Puja kumari <pujakumari11832@gmail.com>
2026-08-23 23:12:49 +0530

    question revision

---
+++ revsion question/3.variable_datatypes.py
@@ -0,0 +1,18 @@
+"""
+Q3. Take a student's name, age, and marks. Print them with their data types
+"""
+name=input("Enter the name")
+age=int(input("Enter your age"))
+marks=int(input("Enter your marks"))
+print(type(name))
+print(type(age))
+print(type(marks))
+
+def print_the_data():
+    name=input("Enter the name")
+    age=int(input("Enter your age"))
+    marks=int(input("Enter your marks"))
+    print(type(name))
+    print(type(age))
+    print(type(marks)) 
+print_the_data()
