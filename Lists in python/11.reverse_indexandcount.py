d1fa6e2987d5b6271b2eac02cf45c4b2a1f328d6
Puja kumari <pujakumari11832@gmail.com>
2026-08-25 14:30:22 +0530

    reverse, index,count done

--- Lists in python/11.reverse_indexandcount.py
+++ Lists in python/11.reverse_indexandcount.py
@@ -7,3 +7,4 @@ nums=[1,4,1,2,5,1,5,3,1,2,1,3,6,1]
 print(nums.index(5))
 #print(nums.index(89))    #error print karega
 print(nums.count(5))
+print(nums.count(300))   #jo value count ke under nhi rahta hai uska output 0 print karta hai.
