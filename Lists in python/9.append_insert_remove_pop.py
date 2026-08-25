c4a84e5405fb7e03f8ca5d85e6f31f5d80a8675a
Puja kumari <pujakumari11832@gmail.com>
2026-08-25 13:58:52 +0530

    append, insert, pop, remove

--- Lists in python/9.append_insert_remove_pop.py
+++ Lists in python/9.append_insert_remove_pop.py
@@ -1,4 +1,21 @@
 lst=["Anirudh", 54,21.98, True, "Surat"]
 print(lst)
-print(lst(lst.append(100)))
-#print(lst)
+lst.append(100)
+print(lst)
+lst.append("Delhi")
+print(lst)
+lst.insert(2,"Delhi")
+print(lst)
+lst.insert(0,"Muskan")
+print(lst)
+
+
+x=lst.pop()
+print(x)
+
+x=lst.pop(2)
+print(x)
+print(lst)
+
+lst.remove(21.98)
+print(lst)
