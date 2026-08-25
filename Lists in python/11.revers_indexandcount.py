121084f961a9e3eb65ee3c4bf401ab3ec285ccdc
Puja kumari <pujakumari11832@gmail.com>
2026-08-25 14:25:01 +0530

    reverse, index, count done

---
+++ Lists in python/11.revers_indexandcount.py
@@ -0,0 +1,9 @@
+nums=[1,4,1,2,5,1,5,3,1,2,1,3,6,1]
+#print(f"nums = {nums}, id(nums)")
+#nums.reverse()
+#print(f"nums = {nums}", id(nums))
+
+
+print(nums.index(5))
+#print(nums.index(89))    #error print karega
+print(nums.count(5))
