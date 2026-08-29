bb7285f6ba00c6cca12a514fb5498b2fd6d124b0
Puja kumari <pujakumari11832@gmail.com>
2026-08-29 22:44:45 +0530

    q21.py

---
+++ numerical question/q21.py
@@ -0,0 +1,13 @@
+"""
+1 2 3 4 5 
+1 2 3 4 
+1 2 3 
+1 2 
+1
+"""
+def pattern_based():
+    for i in range(5,0,-1):
+        for j in range(1,i+1):
+            print(j,end=" ")
+        print()
+pattern_based()
