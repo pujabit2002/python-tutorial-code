ef71c9a3e3cbfb74677cf0a820393152fcf1ed04
Puja kumari <pujakumari11832@gmail.com>
2026-08-29 22:38:33 +0530

    q19.py

---
+++ numerical question/q19.py
@@ -0,0 +1,14 @@
+"""
+1 2 3 4 5
+1 2 3 4 5
+1 2 3 4 5
+1 2 3 4 5
+1 2 3 4 5
+"""
+def pattern_based():
+    for i in range(1,6):
+        for j in range(1,6):
+            print(j,end=" ")
+        print()
+pattern_based()
+        
