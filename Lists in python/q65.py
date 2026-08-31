721d0801f064b0381cddf4aba3d758e193d9c264
Puja kumari <pujakumari11832@gmail.com>
2026-08-31 09:56:34 +0530

    modify this code

--- Lists in python/q65.py
+++ Lists in python/q65.py
@@ -4,7 +4,6 @@ replaces every negative number found in the list with the value 0.
 numbers = [5,-3, 8,-1, 0,-10, 12]
 # Expected output: [5, 0, 8, 0, 0, 0, 12]
 """
-import numbers
 def replace_negative(lst):
     n=len(lst)
     for i in range(0,n):
